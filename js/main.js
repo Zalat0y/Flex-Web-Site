939fa054fca2518af5f4af7be74e690d1ef3e390
Alex <40837219+Zalat0y@users.noreply.github.com>
2019-04-17 15:50:49 +0300

    Add files via upload
    Add site in flex

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var mixer = mixitup ('.container-filter' );
+
+$(document).ready(function () {
+
+ $('a[href^="#"]').click(function () {
+  //Сохраняем значение атрибута href в переменной:
+  var target = $(this).attr('href');
+  $('html, body').animate({
+   scrollTop: $(target).offset().top//можно вычесть высоту меню
+  }, 500);
+  return false;
+ });
+
+});
+
+	   $("a.btn").on("click", function() {
+      alert("Application submitted");
+    });
